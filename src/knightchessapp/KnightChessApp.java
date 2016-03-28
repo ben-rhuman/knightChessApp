38dab62027485e7d0107225fbefb3af78769c098
benrhuman <benrhuman@10.180.149.72>
2016-03-27 18:36:29 -0600

    Base Classes

--- src/knightchessapp/KnightChessApp.java
+++ src/knightchessapp/KnightChessApp.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package knightchessapp;
 
-/**
+/*
+ * Authors: Ben Rhuman, Isacc Sotelo, Brendan Tracey
+ * CSCI 232 - Lab 3
  *
- * @author Ben
  */
-public class KnightChessApp {
+
+public class KnightChessApp { //Start of KnightChessApp Class
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Start of main
         // TODO code application logic here
-    }
+    }//End of main
     
-}
+}//End of KnightChessApp
+
+class Knight {//Starts of Knight class
+    
+}//End of Knight class
+
+class Board {//Start of Board class
+    
+}//End of Board class
+
+
